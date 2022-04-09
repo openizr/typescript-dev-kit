939adc8cd9e5960ec9f77a116eed6036e6b8575d
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2022-04-09 12:40:31 +0200

    Update playground

--- playground/frontend/src/scripts/components/Loader.svelte
+++ playground/frontend/src/scripts/components/Loader.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   export let locale: Record<string, string>;
-  if (locale === { a: 'zadijidj' }) {
-    console.log('joizdjidj)');
-  }
 </script>
 
-<div>{locale.LOADING}</div>
+<div>{JSON.stringify(locale)}</div>
--- playground/frontend/src/scripts/store/routes.ts
+++ playground/frontend/src/scripts/store/routes.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 import { defineAsyncComponent } from 'vue';
 
 interface Routes {
