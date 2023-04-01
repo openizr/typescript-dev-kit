c7936fabb20886db9f09c59890b9504ff53fa399
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2023-04-01 14:47:13 +0200

    Update examples

--- examples/backend/src/scripts/routes/v1/getMessage.ts
+++ examples/backend/src/scripts/routes/v1/getMessage.ts
@@ -2,6 +2,22 @@ import { deepMerge } from 'basx';
 import schema from 'scripts/lib/baseSchema';
 import { FastifyRequest, FastifyReply } from 'fastify';
 
+const routeSchema = deepMerge(
+  schema,
+  {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+        },
+      },
+    },
+  },
+) as any;
+
+delete routeSchema.body;
+
 /**
  * `GET /v1/message` endpoint handler.
  */
@@ -9,18 +25,5 @@ export default {
   handler: (_request: FastifyRequest, response: FastifyReply): void => {
     response.send({ message: 'WELCOME_MESSAGE' });
   },
-  schema: deepMerge(
-    schema,
-    {
-      body: undefined,
-      response: {
-        200: {
-          type: 'object',
-          properties: {
-            message: { type: 'string' },
-          },
-        },
-      },
-    },
-  ),
+  schema: routeSchema,
 };
